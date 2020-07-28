77253148522dd754b30908a75a8df513b5487c45
Ajay <majay1638@gmail.com>
2020-07-28 06:58:21 +0530

    Django app setup

---
+++ my-app/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\desktop\python\amzon-price-tracker-django-app\my-app\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
